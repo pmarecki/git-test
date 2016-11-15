3a2e51dc7809a5151295766f346303623c27f0ec
Piotr Marecki <pmarecki@gmail.com>
2016-11-15 15:38:57 +0100

    Update memory.sh

--- src/main/java/options_and_gc/memory.sh
+++ src/main/java/options_and_gc/memory.sh
@@ -1,12 +1,11 @@
 #Java options and memory profiling
 
 jps
-jstat -gcold <pid> 100 1000
 
 (every 100 ms; 1000 times; OC: current max heap size; OU: current utilization [commitet heap] (-Xmx is max OC achievable [maximum heap]);
 O: % of OC used; FGC: full GC event count; GCT= gc total duration)
  
- Full heap = young generation (eden + rest) + old generation (true long-persistent objects) 
+# Full heap = young generation (eden) + old generation (true long-persistent objects) 
 -XX:NewRatio=3   --> 1GB eden + 3GB old
 ----------------------------
 gc flow: eden >> old
@@ -15,4 +14,15 @@ FGC  = full gc (old)
 ------------------------
 
 jstat -gccapacity <pid>
-OGCMN/OGCMX -- old gen current sizes (together with Eden < Xmx)
+# OGC, OC == size of old gen; OGCMX == max old gen
+# (old + eden < Xmx)
+
+jstat -gcutil <pid>
+# E = eden(%), O = old(%); E^^100% --> YGC event, E=0%; O grows very slowly 
+
+# Nice commands:
+jstat -gcutil -h15 <pid> 500 1000  #watch as E grows, and as O grows slowly
+jstat -gccapacity <pid>            #
+
+
+#O grows to ~85%; then OC start to grow up to OGCMX, then O will grow to 100% and crash may occurr
